f75d1bb308e5ac89e694dbc1cb3b4adc70111c59
Shuai <uif46353@contiwan.com>
2024-11-08 14:25:33 +0800

    init

--- server.ts
+++ server.ts
@@ -1,6 +1,9 @@
 import { createServer } from "http";
 import { parse } from "url";
 import next from "next";
+import { WebSocketServer } from 'ws';
+import {Client} from 'ssh2'
+
 
 const port = parseInt(process.env.PORT || "3000", 10);
 const dev = process.env.NODE_ENV !== "production";
@@ -13,6 +16,26 @@ app.prepare().then(() => {
     handle(req, res, parsedUrl);
   }).listen(port);
 
+const conn = new Client();
+conn.on('ready', () => {
+  console.log('Client :: ready');
+  conn.exec('uptime', (err, stream) => {
+    if (err) throw err;
+    stream.on('close', (code: string, signal: string) => {
+      console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
+      conn.end();
+    }).on('data', (data: string) => {
+      console.log('STDOUT: ' + data);
+    }).stderr.on('data', (data) => {
+      console.log('STDERR: ' + data);
+    });
+  });
+}).connect({
+  host: '192.168.1.77',
+  port: 22,
+  username: 'zhaos',
+  password:'123456'
+});
   console.log(
     `> Server listening at http://localhost:${port} as ${
       dev ? "development" : process.env.NODE_ENV
